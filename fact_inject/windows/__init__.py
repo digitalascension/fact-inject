147847273b9b9ab8eff85c93510f9314ebbfd651
Luke Brady <lbrad23105@gmail.com>
2018-11-15 11:36:03 -0500

    Seperating functions that create sessions, generate environment variables, and inject facts.
    Signed-off-by: Luke Brady <lbrad23105@gmail.com>

--- fact_inject/windows/__init__.py
+++ fact_inject/windows/__init__.py
@@ -1,6 +1,7 @@
 import winrm
 
 # Create the winrm session that will be used to add facts to a windows host.
+# This is a helper function.
 def create_winrm_session(host, username, password):
     try:
         # Create a winrm connection with winrm.
@@ -13,6 +14,15 @@ def create_winrm_session(host, username, password):
         print('Error: Could not connect to {}.'.format(host))
         exit(code=1)
 
+# Generate all of the environment variables that will be used to create the powershell
+# script that wil inject windows environment variables.
+def create_win_facter_vars(facts):
+    vars = []
+    for item in facts.keys():
+        vars.append('$env:{0}="{1}"'.format(item, facts[item]))
+    return vars
+
+# Add Facter facts to windows as environment variables.
 def inject_win_facts(facts, host, username, password):
     cmd = ''
     rm = create_winrm_session(host, username, password)
